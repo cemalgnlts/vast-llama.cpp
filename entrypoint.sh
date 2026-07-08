98d6317e5bf514b6a3569daf61414744815046fc
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2026-07-08 22:12:47 +0300

    Modify entrypoint.sh to use tee for logging
    Redirect output of entrypoint.sh to tee for logging.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,7 @@ cd /workspace
   --top-p "${LLAMA_ARG_TOP_P:-0.95}" \
   --min-p "${LLAMA_ARG_MIN_P:-0.05}" \
   --temp "${LLAMA_ARG_TEMP:-0.80}" \
-  --reasoning-budget-message "I have thought enough. Let me give my answer now." \
   --gpu-layers "${LLAMA_ARG_N_GPU_LAYERS:-all}" \
-  --reasoning-budget 2048 \
   --cache-reuse 256 \
   --flash-attn on \
   --kv-unified \
@@ -17,4 +15,4 @@ cd /workspace
   --no-warmup \
   --no-mmproj \
   --no-slots \
-  --no-ui &> "$MODEL_LOG"
+  --no-ui |& tee "$MODEL_LOG"
